cb2a410faa6cbdc96c41bd712f52629960c11fe7
Karthik Jeeyar <karthik.jk11@gmail.com>
2019-04-02 21:31:59 +0530

    feat(setup_devconsole):Toggle feature flag by fetching from devconsole CRD (#18)
    * add devconsle routes based on feature flags
    
    * fetch CRDs from oc cluster
    
    * add requested changes
    
    * add script to install operator
    
    * remove webpack devconsole config

---
+++ frontend/devconsole.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DEVCONSOLE_OPERATORS_INSTALLED=$(oc get pods --all-namespaces --output=json | jq '[.items | .[].metadata.name  | contains("devops")] | any')
+DEVCONSOLE_CRDS_INSTALLED=$(oc get crds --output=json | jq '[.items | .[].metadata.name  | contains("gitsources.devopsconsole.openshift.io")] | any')
+
+if  $DEVCONSOLE_OPERATORS_INSTALLED && $DEVCONSOLE_CRDS_INSTALLED 
+then
+   echo -e "\n\033[0;32m \xE2\x9C\x94 Devconsole Operator and crds are already installed \033[0m\n"
+else
+
+   echo -e "Installing OLM... \n"
+   oc create -f https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/master/deploy/upstream/quickstart/olm.yaml
+   echo -e "\n Installing DevConsole Operator... \n"
+   oc create -f http://operator-hub-shbose-preview1-stage.b542.starter-us-east-2a.openshiftapps.com/install/devopsconsole.v0.1.0.yaml
+
+fi
