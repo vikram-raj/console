671b63e1e4de09f40fbb333614f485377515476f
Vikram Raj <vikrmrj@gmail.com>
2019-04-04 08:55:17 +0530

    fix(import): add form component from patternfly

--- frontend/public/extend/devconsole/components/import/import-flow.tsx
+++ frontend/public/extend/devconsole/components/import/import-flow.tsx
@@ -1,55 +1,148 @@
 // import * as _ from 'lodash-es';
 import * as React from 'react';
-import { Button } from '@patternfly/react-core';
+import {
+  FormGroup,
+  TextInput,
+  Form,
+  FormSelectOption,
+  FormSelect,
+  ActionGroup,
+  Button
+} from '@patternfly/react-core';
 
 export class ImportFlow extends React.Component<any, any> {
   constructor(props) {
     super(props);
     this.state = {
-      codeBase: null,
-      nameSpace: 'myproject',
-      show: false ,
+      gitType: 'please choose Git type',
+      gitRepoUrl: '',
+      applicationName: '',
+      name: '',
+      builderImage: ''
     };
   }
 
-  // Close modal window
-  toggleOverlay = (event) => this.setState({show: !this.state.show});
+  handleGitTypeChange = (gitType: string, event) => {
+    this.setState({ gitType: gitType });
+  }
+
+  handleGitRepoUrlChange = (gitRepoUrl: string) => {
+    this.setState({ gitRepoUrl: gitRepoUrl });
+  }
+
+  handleApplicationNameChange = (applicationName: string, event) => {
+    this.setState({ applicationName: applicationName })
+  }
+
+  handleNameChange = (name: string) => {
+    this.setState({ name: name });
+  }
+
+  gitTypes = [
+    {
+      value: '',
+      label: 'please choose Git type'
+    },
+    {
+      value: 'github',
+      label: 'GitHub'
+    },
+    {
+      value: 'gitlab',
+      label: 'GitLab'
+    },
+    {
+      value: 'bitbucket',
+      label: 'Bitbucket'
+    }
+  ];
+
+  applicationNames = [
+    {
+      value: '',
+      label: 'please application name'
+    },
+    {
+      value: 'application-1',
+      label: 'Application 1'
+    },
+    {
+      value: 'application-2',
+      label: 'Application 2'
+    }
+  ]
 
   render() {
     return (
-      <div className="modal-body-content">
-        <div className="modal-body-inner-shadow-covers">
-          <div className="form-group">
-            <p>Git</p>
-            <span>Some help text about this section. Lorem Ipsum</span>
-            <label>Git Type</label>
-
-            <label htmlFor="name">Name </label>
-            <input
-              className="form-control"
-              type="text" id="name"
-            />
-          </div>
-          <div className="form-group">
-            <label htmlFor="gitUrl">Git URL</label>
-            <input
-              className="form-control"
-              type="text"
-              id="gitUrl"
-            />
-          </div>
-          <div className="form-group">
-            <h1 className="catalog-item-header-pf-title">Authorize</h1>
-            <div className="import__auth">
-              Authorize us to clone your repo
-              <br/>
-              <Button>Authorize</Button>
-            </div>
-          </div>
-          <div className="form-group">
-            <h1 className="catalog-item-header-pf-title">Select builder image to use</h1>
-          </div>
+      <div>
+        <div>
+          <p>Git</p>
+          <p>
+            Some help text about the section lorem ipsum
+          </p>
         </div>
+        <Form isHorizontal>
+          <FormGroup
+            label='Git Type'
+            isRequired
+            fieldId='import-git-type'
+            helperText='Some help text with explanation'>
+            <FormSelect
+              value={ this.state.gitType }
+              name='gitType'
+              id='import-git-type'
+              onChange={this.handleGitTypeChange}
+              >
+              {this.gitTypes.map((gitType, index) => (
+                <FormSelectOption key={index} value={gitType.value} label={gitType.label} />
+              ))}
+            </FormSelect>
+          </FormGroup>
+          <FormGroup
+            label='Git Repository URL'
+            isRequired
+            fieldId='import-git-repo-url'>
+            <TextInput
+              value={ this.state.gitRepoUrl }
+              onChange={ this.handleGitRepoUrlChange }
+              isRequired
+              type='text'
+              id='import-git-repo-url'
+              name='gitRepoUrl'/>
+          </FormGroup>
+          <FormGroup
+            label='Application Name'
+            isRequired
+            fieldId='import-application-name'
+            helperText='Some helper text'>
+            <FormSelect
+              value={ this.state.applicationName }
+              name='applicationName'
+              id='import-application-name'
+              onChange={ this.handleApplicationNameChange }>
+              {this.applicationNames.map((appName, index) => (
+                <FormSelectOption key={index} value={appName.value} label={appName.label} />
+              ))}
+            </FormSelect>
+          </FormGroup>
+          <FormGroup
+            label='Name'
+            isRequired
+            fieldId='import-name'
+            helperText='Identifies the resources created for this application'>
+            <TextInput
+              value={this.state.name}
+              onChange={ this.handleNameChange }
+              isRequired
+              type='text'
+              id='import-name'
+              name='name'/>
+          </FormGroup>
+          <ActionGroup>
+            <Button variant='primary'>Create</Button>
+            <Button variant='secondary'>Cancel</Button>
+          </ActionGroup>
+        </Form>
       </div>
     )
   }
