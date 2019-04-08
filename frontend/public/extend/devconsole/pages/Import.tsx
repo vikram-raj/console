75aa06a2d0e1b317273f30b838fa83ce9d2b224f
Vikram Raj <vikrmrj@gmail.com>
2019-04-08 16:44:56 +0530

    fix(import): make separate component for import flow form.

--- frontend/public/extend/devconsole/pages/Import.tsx
+++ frontend/public/extend/devconsole/pages/Import.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
-import ImportFlow from './../components/import/import-flow';
-import { Firehose } from './../../../../public/components/utils';
-import { NamespaceModel } from './../../../../public/models';
+import { ImportFlow } from './../components/import/import-flow';
 import './import.scss';
 
 const ImportPage: React.SFC = () => (
   <div className='odc-import-container'>
-    <Firehose resources={[{kind: NamespaceModel.kind, prop: 'namespace', isList: true}]}>
-      <ImportFlow />
-    </Firehose>
+    <ImportFlow />
   </div>
 );
 
