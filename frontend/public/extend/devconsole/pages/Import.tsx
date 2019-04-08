c636314e0ad7abb5abcb0812b12c6d5900026086
Vikram Raj <vikrmrj@gmail.com>
2019-04-08 14:13:50 +0530

    fix(import): PR review changes  - Follow BEM notation for class name  - Rename preview-import -> OverlayImportFlow  - Change OVerlayImportImport component from class to functional component  - Re-arrange the dependecies import

--- frontend/public/extend/devconsole/pages/Import.tsx
+++ frontend/public/extend/devconsole/pages/Import.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import ImportFlow from './../components/import/import-flow';
-import './import.scss';
 import { Firehose } from './../../../../public/components/utils';
 import { NamespaceModel } from './../../../../public/models';
+import './import.scss';
 
 const ImportPage: React.SFC = () => (
-  <div className='import-container'>
+  <div className='odc-import-container'>
     <Firehose resources={[{kind: NamespaceModel.kind, prop: 'namespace', isList: true}]}>
       <ImportFlow />
     </Firehose>
