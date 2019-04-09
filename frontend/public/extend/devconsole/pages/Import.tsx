e9d91a93e9f2e89de5d4ad3f2da10004545aee70
Vikram Raj <vikrmrj@gmail.com>
2019-04-09 08:30:48 +0530

    fix(import): use PF3 form in ImportFlowForm component  - add typeahead dropdown for application name field

--- frontend/public/extend/devconsole/pages/Import.tsx
+++ frontend/public/extend/devconsole/pages/Import.tsx
@@ -3,7 +3,7 @@ import { ImportFlow } from './../components/import/import-flow';
 import './import.scss';
 
 const ImportPage: React.SFC = () => (
-  <div className='odc-import-container'>
+  <div>
     <ImportFlow />
   </div>
 );
