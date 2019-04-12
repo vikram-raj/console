a36f16f2e69a8969b8b18da6a8f4bc482a944345
Vikram Raj <vikrmrj@gmail.com>
2019-04-12 15:38:06 +0530

    fix(import): remove style from ImportFlowForm component and add to parent compnent  - remove unnecessary else condition

--- frontend/public/extend/devconsole/components/import/import-flow.tsx
+++ frontend/public/extend/devconsole/components/import/import-flow.tsx
@@ -2,12 +2,21 @@ import * as React from 'react';
 import { Firehose } from '../../../../components/utils';
 import { NamespaceModel } from '../../../../models';
 import ImportFlowForm from '../import-flow-form/ImportFlowForm';
+import './import-flow.scss';
 
 export const ImportFlow: React.FunctionComponent = () => {
 
   return (
-    <Firehose resources={[{kind: NamespaceModel.kind, prop: 'namespace', isList: true}]}>
-      <ImportFlowForm />
-    </Firehose>
+    <div className="odc-import-flow-wrapper">
+      <h1>
+        Git import
+      </h1>
+      <p>
+        Some help text about the section lorem ipsum
+      </p>
+      <Firehose resources={[{kind: NamespaceModel.kind, prop: 'namespace', isList: true}]}>
+        <ImportFlowForm />
+      </Firehose>
+    </div>
   );
 };
