679fa258fa6f93264c914e51a9de7f8fd63a3796
Vikram Raj <vikrmrj@gmail.com>
2019-04-15 15:18:48 +0530

    fix(import): add cancle button functionality

--- frontend/public/extend/devconsole/components/import-flow-form/ImportFlowForm.tsx
+++ frontend/public/extend/devconsole/components/import-flow-form/ImportFlowForm.tsx
@@ -30,6 +30,19 @@ interface Props {
   };
 }
 
+const initialState: State = {
+  gitType: '',
+  gitRepoUrl: '',
+  applicationName: '',
+  name: '',
+  builderImage: '',
+  gitTypeError: '',
+  gitRepoUrlError: '',
+  applicationNameError: '',
+  nameError: '',
+  builderImageError: '',
+}
+
 export class ImportFlowForm extends React.Component<Props, State> {
   constructor(props) {
     super(props);
@@ -139,6 +152,12 @@ export class ImportFlowForm extends React.Component<Props, State> {
 
   }
 
+  handleCancle = (event) => {
+    event.preventDefault();
+    this.setState(initialState);
+    history.back();
+  }
+
   autocompleteFilter = (text, item) => fuzzy(text, item);
 
   render() {
@@ -236,7 +255,7 @@ export class ImportFlowForm extends React.Component<Props, State> {
         </FormGroup>
         <div className="co-m-btn-bar">
           <Button type="submit" bsStyle="primary">Create</Button>
-          <Button type="button">Cancel</Button>
+          <Button type="button" onClick={this.handleCancle}>Cancel</Button>
         </div>
       </Form>
     );
