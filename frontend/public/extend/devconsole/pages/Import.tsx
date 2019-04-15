6224831ca1cbc3df1ea32257ea27b1f5bb01feaf
Vikram Raj <vikrmrj@gmail.com>
2019-04-15 08:55:48 +0530

    fix(import): addressed review comment  - change detectGitType function type  - validate valid URL onChange  - add namespace bar

--- frontend/public/extend/devconsole/pages/Import.tsx
+++ frontend/public/extend/devconsole/pages/Import.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
+import { PageSection, PageSectionVariants } from '@patternfly/react-core';
+import { NamespaceBar } from './../../../../public/components/namespace';
 import { ImportFlow } from './../components/import/import-flow';
 import './import.scss';
 
 const ImportPage: React.FunctionComponent = () => (
-  <div>
-    <ImportFlow />
-  </div>
+  <PageSection variant={PageSectionVariants.light}>
+    <NamespaceBar />
+    <div className="odc-import-page-wrapper">
+      <ImportFlow />
+    </div>
+  </PageSection>
 );
 
 export default ImportPage;
