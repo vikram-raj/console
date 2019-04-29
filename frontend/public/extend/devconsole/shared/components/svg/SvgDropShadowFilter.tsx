3dd2fa4832cfb8cf6692d1f82da71c1bf81d7c36
christianvogt <cvogt@redhat.com>
2019-04-28 22:04:57 -0400

    reusable SVG drop shadow component

---
+++ frontend/public/extend/devconsole/shared/components/svg/SvgDropShadowFilter.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable no-unused-vars, no-undef */
+import * as React from 'react';
+import SvgDefs from '../../../shared/components/svg/SvgDefs';
+
+interface SvgDropShadowFilterProps {
+  /* The unique ID that identifies the filter.
+     It is also used to uniquely identify the def entry to prevent duplicates. */
+  id: string;
+  dx?: number;
+  dy?: number;
+  stdDeviation?: number;
+  floodOpacity?: number;
+  floodColor?: string;
+}
+
+const SvgDropShadowFilter: React.FC<SvgDropShadowFilterProps> = ({
+  id,
+  dx = 0,
+  dy = 1,
+  stdDeviation = 1,
+  floodColor = '#030303',
+  floodOpacity = 0.15,
+}) => (
+  <SvgDefs id={id}>
+    <filter
+      id={id}
+      x={`-${stdDeviation * 12.5}%`}
+      y={`-${stdDeviation * 12.5}%`}
+      width={`${100 + stdDeviation * 25}%`}
+      height={`${100 + stdDeviation * 25}%`}
+    >
+      {/*
+        // @ts-ignore */}
+      <feDropShadow
+        dx={dx}
+        dy={dy}
+        stdDeviation={stdDeviation}
+        floodColor={floodColor}
+        floodOpacity={floodOpacity}
+      />
+    </filter>
+  </SvgDefs>
+);
+
+export default SvgDropShadowFilter;
