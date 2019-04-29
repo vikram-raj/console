73a5e4ad890a73cb77cb7aeca7e599c4be7381e5
christianvogt <cvogt@redhat.com>
2019-04-28 22:04:03 -0400

    SVG defs component which renders nested child defs as a direct child of the SVG itself

---
+++ frontend/public/extend/devconsole/shared/components/svg/SvgDefsContext.ts
@@ -0,0 +1,11 @@
+/* eslint-disable no-unused-vars, no-undef */
+import { createContext, ReactNode } from 'react';
+
+export interface SvgDefsContextProps {
+  addDef(id: string, node: ReactNode): void;
+  removeDef(id: string): void;
+}
+
+const SvgDefsContext = createContext<SvgDefsContextProps>(undefined);
+
+export default SvgDefsContext;
