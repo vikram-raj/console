97324edf51a6b069afcde8f8113fe274a89e2c66
Christian Vogt <cvogt@redhat.com>
2019-05-21 14:24:37 -0400

    improve topology grouping convex hull (#105)

--- frontend/public/extend/devconsole/components/topology/shapes/DefaultGroup.tsx
+++ frontend/public/extend/devconsole/components/topology/shapes/DefaultGroup.tsx
@@ -1,21 +1,20 @@
 /* eslint-disable no-unused-vars, no-undef */
 import * as React from 'react';
 import * as d3 from 'd3';
-import { GroupProps, ViewNode } from '../topology-types';
+import * as _ from 'lodash-es';
+import { GroupProps } from '../topology-types';
 import SvgBoxedText from '../../../shared/components/svg/SvgBoxedText';
+import { hullPath } from '../../../shared/utils/svg-utils';
 import './DefaultGroup.scss';
 
-const valueline = d3
-  .line()
-  .x((d) => d[0])
-  .y((d) => d[1])
-  .curve(d3.curveBasisClosed);
+type Point = [number, number];
+type PointWithSize = Point & [number, number, number];
 
 // Return the point whose Y is the largest value.
-function findLowestPoint(points: [number, number][]): [number, number] {
+function findLowestPoint<P extends Point>(points: P[]): P {
   let lowestPoint = points[0];
 
-  points.forEach((p) => {
+  _.forEach(points, (p) => {
     if (p[1] > lowestPoint[1]) {
       lowestPoint = p;
     }
@@ -24,68 +23,32 @@ function findLowestPoint(points: [number, number][]): [number, number] {
   return lowestPoint;
 }
 
-// Padding around group points in pixels.
-const GP = 50;
-
-// Returns an array of points representing 8 points around the center of the node.
-// We use these points to ensure that the surrounding group box does not intersect with the node.
-function getNodePoints(node: ViewNode): [number, number][] {
-  const radius = node.size / 2;
-  const x0 = node.x - radius;
-  const x1 = node.x + radius;
-  const y0 = node.y - radius;
-  const y1 = node.y + radius;
-  return [
-    ...[45, 135, 225, 315].map(
-      (θ) =>
-        [
-          node.x + (radius + GP) * Math.sin(θ * Math.PI / 180),
-          node.y + (radius + GP) * Math.cos(θ * Math.PI / 180),
-        ] as [number, number],
-    ),
-    [node.x, y0 - GP],
-    [node.x, y1 + GP],
-    [x0 - GP, node.y],
-    [x1 + GP, node.y],
-  ];
-}
+const hullPadding = (point: PointWithSize) => point[2] + 40;
 
 const DefaultGroup: React.FC<GroupProps> = ({ name, nodes }) => {
   if (nodes.length === 0) {
     return null;
   }
 
-  // collect all points to avoid for all nodes
-  const points = nodes.reduce((acc, node) => [...acc, ...getNodePoints(node)], [] as [
-    number,
-    number
-  ][]);
+  // convert nodes to points
+  const points: PointWithSize[] = new Array(nodes.length);
+  _.forEach(nodes, (node, i) => {
+    points[i] = [node.x, node.y, node.size / 2] as PointWithSize;
+  });
 
   // Get the convex hull of all points.
-  const polygon = d3.polygonHull(points);
-
-  // Find the cenroid of the convex hull
-  const centroid = d3.polygonCentroid(polygon);
-
-  // Convert the convex hull to a set of points to be used for the surrounding path.
-  const dPoints = polygon.map(
-    (point) => [point[0] - centroid[0], point[1] - centroid[1]] as [number, number],
-  );
+  const d = hullPath(points.length > 2 ? d3.polygonHull(points) : points, hullPadding);
 
   // Find the lowest point of the set in order to place the group label.
-  const lowestPoint = findLowestPoint(dPoints);
+  const lowestPoint = findLowestPoint(points);
 
   return (
     <g>
-      <path
-        d={valueline(dPoints)}
-        className="odc-default-group"
-        transform={`translate(${centroid[0]},${centroid[1]})`}
-      />
+      <path d={d} className="odc-default-group" />
       <SvgBoxedText
         className={'odc-default-group__label'}
-        x={centroid[0] + lowestPoint[0]}
-        y={centroid[1] + lowestPoint[1] + 30}
+        x={lowestPoint[0]}
+        y={lowestPoint[1] + hullPadding(lowestPoint) + 30}
         paddingX={20}
         paddingY={5}
       >
