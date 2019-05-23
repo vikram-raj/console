4ad155b4b1ff0651731142502f8d9ded0f3b506c
Christian Vogt <cvogt@redhat.com>
2019-05-22 23:14:00 -0400

    fix(pod): smooth out the pod animation transitions between phases (#113)

--- frontend/public/extend/devconsole/components/topology/shapes/PodStatus.tsx
+++ frontend/public/extend/devconsole/components/topology/shapes/PodStatus.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars, no-undef, no-shadow */
 import * as React from 'react';
+import * as _ from 'lodash-es';
 import { VictoryPie } from 'victory';
 import { Pod } from '../topology-types';
 import Tooltip from './../SvgPodTooltip';
@@ -21,79 +22,74 @@ type PodStatusProps = {
   showTooltip?: boolean;
 };
 
-const PodStatus: React.FC<PodStatusProps> = ({
-  innerRadius,
-  outerRadius,
-  x,
-  y,
-  data,
-  size,
-  standalone = false,
-  showTooltip = true,
-}) => {
-  const vData: PodData[] = [];
-  podStatus.forEach((pod) => {
-    let podNumber = 0;
-    data.forEach((element) => {
-      if (getPodStatus(element) === pod) {
-        podNumber += 1;
-      }
-    });
-    if (podNumber !== 0) {
-      vData.push({ x: pod, y: podNumber });
-    }
-  });
-  const tooltipEvent = showTooltip
-    ? [
-      {
-        target: 'data',
-        eventHandlers: {
-          onMouseOver: () => {
-            return [
-              {
-                target: 'labels',
-                mutation: (props) => {
-                  return { active: true };
+class PodStatus extends React.PureComponent<PodStatusProps> {
+  render() {
+    const {
+      innerRadius,
+      outerRadius,
+      x,
+      y,
+      data,
+      size,
+      standalone = false,
+      showTooltip = true,
+    } = this.props;
+    const vData: PodData[] = podStatus.map((pod) => ({
+      x: pod,
+      y: _.sumBy(data, (d) => +(getPodStatus(d) === pod)) || 0,
+    }));
+    const tooltipEvent = showTooltip
+      ? [
+        {
+          target: 'data',
+          eventHandlers: {
+            onMouseOver: () => {
+              return [
+                {
+                  target: 'labels',
+                  mutation: () => {
+                    return { active: true };
+                  },
                 },
-              },
-            ];
-          },
-          onMouseOut: () => {
-            return [
-              {
-                target: 'labels',
-                mutation: (props) => {
-                  return { active: false };
+              ];
+            },
+            onMouseOut: () => {
+              return [
+                {
+                  target: 'labels',
+                  mutation: () => {
+                    return { active: false };
+                  },
                 },
-              },
-            ];
+              ];
+            },
           },
         },
-      },
-    ]
-    : undefined;
-  return (
-    <VictoryPie
-      events={tooltipEvent}
-      animate={{
-        duration: 2000,
-      }}
-      standalone={standalone}
-      innerRadius={innerRadius}
-      radius={outerRadius}
-      groupComponent={x && y ? <g transform={`translate(${x}, ${y})`} /> : undefined}
-      data={vData}
-      height={size}
-      width={size}
-      labelComponent={<Tooltip x={size / 2} y={-12} />}
-      padAngle={2}
-      style={{
-        data: {
-          fill: ({ x }) => podColor[x],
-        },
-      }}
-    />
-  );
-};
+      ]
+      : undefined;
+    return (
+      <VictoryPie
+        events={tooltipEvent}
+        animate={{
+          duration: 2000,
+        }}
+        standalone={standalone}
+        innerRadius={innerRadius}
+        radius={outerRadius}
+        groupComponent={x && y ? <g transform={`translate(${x}, ${y})`} /> : undefined}
+        data={vData}
+        height={size}
+        width={size}
+        labelComponent={<Tooltip x={size / 2} y={-12} />}
+        padAngle={(d: PodData) => (d.y > 0 ? 2 : 0)}
+        style={{
+          data: {
+            fill: (d: PodData) => podColor[d.x],
+          },
+        }}
+      />
+    );
+  }
+}
 
 export default PodStatus;
