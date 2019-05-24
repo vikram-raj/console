d84f27f60069d8400b0777114606223ea1904475
Jaivardhan <mailjai.vardhan@gmail.com>
2019-05-24 07:33:48 +0530

    fix(page-layout): adds custom styles to handle page layout (#115)

--- frontend/public/extend/devconsole/shared/components/EmptyState/EmptyState.tsx
+++ frontend/public/extend/devconsole/shared/components/EmptyState/EmptyState.tsx
@@ -26,76 +26,78 @@ export type ODCEmptyStateProps = StateProps & ODCEmptyStateOwnProps;
 
 const ODCEmptyState: React.FunctionComponent<ODCEmptyStateProps> = (props: ODCEmptyStateProps) => (
   <React.Fragment>
-    <PageHeading title={props.title} />
     <div className="odc-empty-state">
-      <Grid gutter="md">
-        <GridItem sm={6} md={6} lg={4}>
-          <Card className="odc-empty-state__card">
-            <CardHeader>Import from Git</CardHeader>
-            <CardBody>Import code from your git repository, to be built and deployed </CardBody>
-            <CardFooter>
-              <PerspectiveLink className="pf-c-button pf-m-secondary" to="/import">
-                Import from Git
-              </PerspectiveLink>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem sm={6} md={6} lg={4}>
-          <Card className="odc-empty-state__card">
-            <CardHeader>Browse Catalog</CardHeader>
-            <CardBody>Browse the catalog to discover, deploy and connect to services</CardBody>
-            <CardFooter>
-              <PerspectiveLink className="pf-c-button pf-m-secondary" to="/catalog">
-                Browse Catalog
-              </PerspectiveLink>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem sm={6} md={6} lg={4}>
-          <Card className="odc-empty-state__card">
-            <CardHeader>Deploy Image</CardHeader>
-            <CardBody>Deploy an existing image from an image registry or image stream tag</CardBody>
-            <CardFooter>
-              <PerspectiveLink
-                className="pf-c-button pf-m-secondary"
-                to={`/deploy-image?preselected-ns=${props.activeNamespace}`}
-              >
-                Deploy Image
-              </PerspectiveLink>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem sm={6} md={6} lg={4}>
-          <Card className="odc-empty-state__card">
-            <CardHeader>Import YAML</CardHeader>
-            <CardBody>Create or replace resources from their YAML or JSON definitions.</CardBody>
-            <CardFooter>
-              <PerspectiveLink
-                className="pf-c-button pf-m-secondary"
-                to={formatNamespacedRouteForResource('import', props.activeNamespace)}
-              >
-                Import YAML
-              </PerspectiveLink>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem sm={6} md={6} lg={4}>
-          <Card className="odc-empty-state__card">
-            <CardHeader>Add Database</CardHeader>
-            <CardBody>
-              Browse the catalog to discover database services to add to your application
-            </CardBody>
-            <CardFooter>
-              <PerspectiveLink
-                className="pf-c-button pf-m-secondary"
-                to="/catalog?category=databases"
-              >
-                Add Database
-              </PerspectiveLink>
-            </CardFooter>
-          </Card>
-        </GridItem>
-      </Grid>
+      <PageHeading title={props.title} />
+      <div className="odc-empty-state__content">
+        <Grid gutter="md">
+          <GridItem sm={6} md={6} lg={4}>
+            <Card className="odc-empty-state__card">
+              <CardHeader>Import from Git</CardHeader>
+              <CardBody>Import code from your git repository, to be built and deployed </CardBody>
+              <CardFooter>
+                <PerspectiveLink className="pf-c-button pf-m-secondary" to="/import">
+                  Import from Git
+                </PerspectiveLink>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem sm={6} md={6} lg={4}>
+            <Card className="odc-empty-state__card">
+              <CardHeader>Browse Catalog</CardHeader>
+              <CardBody>Browse the catalog to discover, deploy and connect to services</CardBody>
+              <CardFooter>
+                <PerspectiveLink className="pf-c-button pf-m-secondary" to="/catalog">
+                  Browse Catalog
+                </PerspectiveLink>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem sm={6} md={6} lg={4}>
+            <Card className="odc-empty-state__card">
+              <CardHeader>Deploy Image</CardHeader>
+              <CardBody>Deploy an existing image from an image registry or image stream tag</CardBody>
+              <CardFooter>
+                <PerspectiveLink
+                  className="pf-c-button pf-m-secondary"
+                  to={`/deploy-image?preselected-ns=${props.activeNamespace}`}
+                >
+                  Deploy Image
+                </PerspectiveLink>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem sm={6} md={6} lg={4}>
+            <Card className="odc-empty-state__card">
+              <CardHeader>Import YAML</CardHeader>
+              <CardBody>Create or replace resources from their YAML or JSON definitions.</CardBody>
+              <CardFooter>
+                <PerspectiveLink
+                  className="pf-c-button pf-m-secondary"
+                  to={formatNamespacedRouteForResource('import', props.activeNamespace)}
+                >
+                  Import YAML
+                </PerspectiveLink>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem sm={6} md={6} lg={4}>
+            <Card className="odc-empty-state__card">
+              <CardHeader>Add Database</CardHeader>
+              <CardBody>
+                Browse the catalog to discover database services to add to your application
+              </CardBody>
+              <CardFooter>
+                <PerspectiveLink
+                  className="pf-c-button pf-m-secondary"
+                  to="/catalog?category=databases"
+                >
+                  Add Database
+                </PerspectiveLink>
+              </CardFooter>
+            </Card>
+          </GridItem>
+        </Grid>
+      </div>
     </div>
   </React.Fragment>
 );
