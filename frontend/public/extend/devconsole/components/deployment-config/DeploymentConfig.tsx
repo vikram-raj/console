a8bc45a35ef0244a5540266c18af654c541f85cb
Vikram Raj <vikrmrj@gmail.com>
2019-06-05 18:20:30 +0530

    add deployment config option component

---
+++ frontend/public/extend/devconsole/components/deployment-config/DeploymentConfig.tsx
@@ -0,0 +1,81 @@
+/*eslint-disable no-unused-vars, no-undef */
+import * as React from 'react';
+import { Checkbox } from './../../../../../public/components/checkbox';
+import { EnvironmentPage } from './../../../../../public/components/environment';
+
+interface NameValueType {
+  name: string;
+  value: string;
+}
+
+interface NameValueFormType {
+  name: string;
+  valueForm: {
+    configMapKeyRef: {
+      key: string;
+      name: string
+    }
+  }
+}
+
+interface BuildConfigProps {
+  onNewImageAvailableChange: React.ReactEventHandler<HTMLInputElement>;
+  onDeploymentConfigChange: React.ReactEventHandler<HTMLInputElement>;
+  onEnviromentVariableChange: (obj: NameValueType | NameValueFormType) => void;
+  newImageAvailableChecked: boolean;
+  deploymentConfigChecked: boolean;
+  namespace: string;
+}
+
+const DeploymentConfig: React.FC<BuildConfigProps> = ({onNewImageAvailableChange,
+  onDeploymentConfigChange,
+  onEnviromentVariableChange,
+  newImageAvailableChecked,
+  deploymentConfigChecked,
+  namespace}) => {
+
+  const buildConfigObj = {
+    metadata: {
+      namespace: namespace
+    }
+  };
+
+  return (
+    <React.Fragment>
+      <div className="co-section-heading">
+        Deployment Configuration
+      </div>
+      <div className="co-section-heading-tertiary">
+        Autodeploy when
+      </div>
+
+      <Checkbox
+        label="New image is available"
+        name="newImageAvailable"
+        onChange={onNewImageAvailableChange}
+        checked={newImageAvailableChecked} />
+
+      <Checkbox
+        label="Deployment configuration changes"
+        name="deploymentConfigurationChange"
+        onChange={onDeploymentConfigChange}
+        checked={deploymentConfigChecked} />
+
+      <div>
+        <div className="co-section-heading-tertiary">
+          Enviroment Variables (Build and Runtime)
+        </div>
+        <div>
+          <EnvironmentPage
+            obj={buildConfigObj}
+            envPath={['spec','template','spec','containers']}
+            readOnly={false}
+            onChange={onEnviromentVariableChange}
+            addConfigMapSecret={true}
+            useLoadingInline={true} />
+        </div>
+      </div>
+    </React.Fragment>);
+};
+
+export default DeploymentConfig;
