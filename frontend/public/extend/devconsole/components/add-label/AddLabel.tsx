256899cf74bdcbf233c237fc96db2ee02c288564
Vikram Raj <vikrmrj@gmail.com>
2019-06-10 01:16:19 +0530

    add props to pass back the value to component and use name-value-editor component

--- frontend/public/extend/devconsole/components/add-label/AddLabel.tsx
+++ frontend/public/extend/devconsole/components/add-label/AddLabel.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-undef, dot-notation */
 import * as React from 'react';
-import { EnvironmentPage } from './../../../../components/environment';
+import * as _ from 'lodash-es';
+import { AsyncComponent } from './../../../../components/utils';
 
 interface LabelValue {
   name: string;
@@ -9,20 +10,26 @@ interface LabelValue {
 
 interface AddLabelProps {
   onLabelAdd: (obj: LabelValue) => void;
+  labels: { [name: string]: string };
+  readOnly: boolean;
 }
 
-const AddLabel: React.FC<AddLabelProps> = ({onLabelAdd}) => {
+const NameValueEditorComponent = (props) => <AsyncComponent loader={() => import('./../../../../components/utils/name-value-editor').then(c => c.NameValueEditor)} {...props} />;
+
+const AddLabel: React.FC<AddLabelProps> = ({onLabelAdd, labels, readOnly}) => {
+
+  const _labels = _.isEmpty(labels) ? [['', '']] : _.toPairs(labels);
 
   return (
     <React.Fragment>
       <div className="co-section-heading">Labels</div>
       <div>Each label is applied to each created resource.</div>
-      <EnvironmentPage
-        envPath={['','']}
-        readOnly={false}
-        onChange={onLabelAdd}
-        addConfigMapSecret={false}
-        useLoadingInline={true} />
+      <NameValueEditorComponent
+        nameValuePairs={_labels}
+        updateParentData={onLabelAdd}
+        useLoadingInline={true}
+        readOnly={readOnly}
+      />
     </React.Fragment>
   );
 };
