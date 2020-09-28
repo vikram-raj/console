9ef6f03ac0a7c25bda7485d881643370f5b8802e
Vikram Raj <vikrmrj@gmail.com>
2020-09-28 17:29:22 +0530

    fix silence toggle in devconsole monitoring

--- frontend/packages/dev-console/src/components/monitoring/alerts/SilenceAlert.tsx
+++ frontend/packages/dev-console/src/components/monitoring/alerts/SilenceAlert.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
 import * as _ from 'lodash';
+// FIXME upgrading redux types is causing many errors at this time
+// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+// @ts-ignore
+import { useSelector, useDispatch } from 'react-redux';
 import { Switch } from '@patternfly/react-core';
-import { Rule, RuleStates } from '@console/internal/components/monitoring/types';
+import { AlertStates, Rule, RuleStates } from '@console/internal/components/monitoring/types';
 import { StateTimestamp } from '@console/internal/components/monitoring/alerting';
 import { coFetchJSON } from '@console/internal/co-fetch';
 import SilenceDurationDropDown from './SilenceDurationDropdown';
+import { RootState } from '@console/internal/redux';
+import { monitoringSetRules } from '@console/internal/actions/ui';
+import { errorModal } from '@console/internal/components/modals';
 
 type SilenceAlertProps = {
   rule: Rule;
@@ -20,14 +27,35 @@ const SilenceUntil = ({ rule }) => {
 };
 
 const SilenceAlert: React.FC<SilenceAlertProps> = ({ rule }) => {
-  const [isChecked, setIsChecked] = React.useState(rule.state !== RuleStates.Silenced);
-  const [ruleState] = React.useState(rule);
-  React.useEffect(() => setIsChecked(ruleState.state !== RuleStates.Silenced), [ruleState]);
+  const [isChecked, setIsChecked] = React.useState(true);
+  const rules = useSelector((state: RootState) => state.UI.getIn(['monitoring', 'devRules']));
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    if (rule.state === RuleStates.Silenced) {
+      setIsChecked(false);
+    }
+  }, [rule]);
 
   const handleChange = (checked: boolean) => {
     if (checked) {
       _.each(rule.silencedBy, (silence) => {
-        coFetchJSON.delete(`${alertManagerBaseURL}/api/v2/silence/${silence.id}`);
+        coFetchJSON
+          .delete(`${alertManagerBaseURL}/api/v2/silence/${silence.id}`)
+          .then(() => {
+            _.each(rule.alerts, (a) => {
+              _.omit(a, 'silencedBy');
+              a.state = AlertStates.NotFiring;
+            });
+            _.omit(rule, 'silencedBy');
+            rule.state = RuleStates.Inactive;
+            const updatedRules = [...rules, ...[rule]];
+            dispatch(monitoringSetRules('devRules', updatedRules, 'dev'));
+            setIsChecked(true);
+          })
+          .catch((err) => {
+            errorModal({ error: err?.message });
+            setIsChecked(false);
+          });
       });
     }
     setIsChecked(checked);
@@ -39,7 +67,7 @@ const SilenceAlert: React.FC<SilenceAlertProps> = ({ rule }) => {
       className="odc-silence-alert"
       label={null}
       labelOff={
-        rule.state === RuleStates.Silenced ? (
+        rule.state === RuleStates.Silenced && !isChecked ? (
           <SilenceUntil rule={rule} />
         ) : (
           <SilenceDurationDropDown rule={rule} />
--- frontend/packages/dev-console/src/components/monitoring/alerts/SilenceDurationDropdown.tsx
+++ frontend/packages/dev-console/src/components/monitoring/alerts/SilenceDurationDropdown.tsx
@@ -3,20 +3,28 @@ import * as _ from 'lodash';
 // FIXME upgrading redux types is causing many errors at this time
 // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
 // @ts-ignore
-import { useSelector } from 'react-redux';
-import { Dropdown } from '@console/internal/components/utils';
+import { useSelector, useDispatch } from 'react-redux';
+import { Dropdown, LoadingInline } from '@console/internal/components/utils';
 import { RootState } from '@console/internal/redux';
 import { parsePrometheusDuration } from '@console/internal/components/utils/datetime';
 import { coFetchJSON } from '@console/internal/co-fetch';
-import { Rule } from '@console/internal/components/monitoring/types';
+import {
+  AlertStates,
+  Rule,
+  RuleStates,
+  SilenceStates,
+} from '@console/internal/components/monitoring/types';
 import { ALERT_MANAGER_TENANCY_BASE_PATH } from '@console/internal/components/graphs';
+import { monitoringSetRules } from '@console/internal/actions/ui';
+import { isSilenced } from '@console/internal/reducers/monitoring';
+import { errorModal } from '@console/internal/components/modals/error-modal';
+import './SilenceDurationDropdown.scss';
 
 type SilenceDurationDropDownProps = {
   rule: Rule;
 };
 
 const durations = {
-  silenceFor: 'Silence for',
   '30m': '30 minutes',
   '1h': '1 hour',
   '2h': '2 hours',
@@ -24,8 +32,11 @@ const durations = {
 };
 
 const SilenceDurationDropDown: React.FC<SilenceDurationDropDownProps> = ({ rule }) => {
+  const [silencing, setSilencing] = React.useState(false);
   const createdBy = useSelector((state: RootState) => state.UI.get('user')?.metadata?.name);
+  const rules = useSelector((state: RootState) => state.UI.getIn(['monitoring', 'devRules']));
   const ruleMatchers = _.map(rule?.labels, (value, key) => ({ isRegex: false, name: key, value }));
+  const dispatch = useDispatch();
 
   const matchers = [
     {
@@ -37,29 +48,58 @@ const SilenceDurationDropDown: React.FC<SilenceDurationDropDownProps> = ({ rule 
   ];
 
   const setDuration = (duration: string) => {
-    if (duration !== 'silenceFor') {
-      const startsAt = new Date();
-      const endsAt = new Date(startsAt.getTime() + parsePrometheusDuration(duration));
+    const startsAt = new Date();
+    const endsAt = new Date(startsAt.getTime() + parsePrometheusDuration(duration));
 
-      const payload = {
-        createdBy,
-        endsAt: endsAt.toISOString(),
-        startsAt: startsAt.toISOString(),
-        matchers,
-        comment: '',
-      };
-
-      coFetchJSON.post(`${ALERT_MANAGER_TENANCY_BASE_PATH}/api/v2/silences`, payload);
-    }
+    const payload = {
+      createdBy,
+      endsAt: endsAt.toISOString(),
+      startsAt: startsAt.toISOString(),
+      matchers,
+      comment: '',
+    };
+    setSilencing(true);
+    coFetchJSON
+      .post(`${ALERT_MANAGER_TENANCY_BASE_PATH}/api/v2/silences`, payload)
+      .then(() => {
+        // eslint-disable-next-line promise/no-nesting
+        coFetchJSON(`${ALERT_MANAGER_TENANCY_BASE_PATH}/api/v2/silences`)
+          .then((silences) => {
+            rule.silencedBy = _.filter(
+              silences,
+              (s) => s.status.state === SilenceStates.Active && _.some(rule.alerts, isSilenced),
+            );
+            if (!_.isEmpty(rule.silencedBy)) {
+              _.each(rule.alerts, (a) => (a.state = AlertStates.Silenced));
+              rule.state = RuleStates.Silenced;
+            }
+            const updatedRules = [...rules, ...[rule]];
+            dispatch(monitoringSetRules('devRules', updatedRules, 'dev'));
+            setSilencing(false);
+          })
+          .catch((err) => {
+            setSilencing(false);
+            errorModal({ error: err?.message });
+          });
+      })
+      .catch((err) => {
+        setSilencing(false);
+        errorModal({ error: err?.message });
+      });
   };
 
   return (
-    <Dropdown
-      dropDownClassName="dropdown--full-width"
-      items={durations}
-      onChange={(v: string) => setDuration(v)}
-      selectedKey={'silenceFor'}
-    />
+    <>
+      <Dropdown
+        dropDownClassName="dropdown--full-width"
+        className="odc-silence-duration-dropdown"
+        items={durations}
+        onChange={(v: string) => setDuration(v)}
+        selectedKey={'silenceFor'}
+        title="Silence for"
+      />
+      {silencing && <LoadingInline />}
+    </>
   );
 };
 
